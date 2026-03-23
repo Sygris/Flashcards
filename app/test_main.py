56094215105ba5bafcead1f7b0fc4df8f3ae67d8
Sygris <tiago.a.vista@gmail.com>
2026-03-23 20:13:54 +0100

    Started to learn how to test my API using pytest and HTTPX

---
+++ app/test_main.py
@@ -0,0 +1,16 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_read_health():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
+
+
+def test_read_health_fail():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json() == {"health": "ok"}
