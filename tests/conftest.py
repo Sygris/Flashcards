8adf1d2258faaeaa0a273bab4dcf4018786a2f1a
Sygris <tiago.a.vista@gmail.com>
2026-03-25 19:21:48 +0100

    Created pytest Fixtures for testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,52 @@
+import json
 import os
+import pytest
+from fastapi.testclient import TestClient
+
+from app.main import app
+from app.db.database import SessionLocal
+from app.db.models.deck import Deck
+from app.db.models.flashcard import Flashcard
+from app.db.models.user import User
+from app.db.utils.init_db import create_tables
 
 os.environ["DB_NAME"] = "flashcards_test"
 os.environ["DB_PORT"] = "5433"
+
+
+@pytest.fixture
+def db_setup():
+    create_tables()
+    session = SessionLocal()
+    yield session
+
+    session.query(Flashcard).delete()
+    session.query(Deck).delete()
+    session.query(User).delete()
+
+    session.commit()
+    session.close()
+
+
+@pytest.fixture
+def client():
+    client = TestClient(app)
+    return client
+
+
+@pytest.fixture
+def access_token(db_setup, client):
+    response = client.post(
+        "/auth/signup", json={"email": "user@gmail.com", "password": "password"}
+    )
+    if response.status_code != 201:
+        pytest.fail("Failed to signup!")
+
+    login_response = client.post(
+        "/auth/login", json={"email": "user@gmail.com", "password": "password"}
+    )
+
+    if login_response.status_code != 200:
+        pytest.fail("Failed to login!")
+
+    return login_response.json()["token"]
