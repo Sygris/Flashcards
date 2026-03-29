0fe39f63beb58bb503d479bfcafa079326fc583f
Sygris <tiago.a.vista@gmail.com>
2026-03-29 20:41:02 +0200

    Created tests for getting flashcards

--- tests/test_flashcard.py
+++ tests/test_flashcard.py
@@ -41,3 +41,36 @@ def test_create_duplicated_flashcard(client, access_token, deck, flashcard):
 
     assert response.status_code == 400
     assert response.json()["detail"] == "Flashcard already exists"
+
+
+def test_list_flashcards(client, access_token, deck, flashcard):
+    response = client.get(
+        f"/decks/{deck['id']}/flashcards/",
+        headers={"Authorization": f"Bearer {access_token}"},
+    )
+
+    assert response.status_code == 200
+    assert flashcard in response.json()
+
+
+def test_list_flashcards_deck_not_found(client, access_token, deck):
+    response = client.get(
+        f"/decks/{deck['id'] + 1}/flashcards/",
+        headers={"Authorization": f"Bearer {access_token}"},
+    )
+
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Deck not found"
+
+
+def test_get_flashcard(client, access_token, deck, flashcard):
+    response = client.get(
+        f"/decks/{deck['id']}/flashcards/{flashcard['id']}",
+        headers={"Authorization": f"Bearer {access_token}"},
+    )
+
+    assert response.status_code == 200
+
+    response_data = response.json()
+    for key in response_data:
+        assert response_data[key] == flashcard[key]
