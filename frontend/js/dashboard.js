2a1932c893b35da740b6845f05faf06611d3f531
Sygris <tiago.a.vista@gmail.com>
2026-04-20 18:55:59 +0200

    Clean up dashboard.js

--- frontend/js/dashboard.js
+++ frontend/js/dashboard.js
@@ -7,58 +7,39 @@ async function refreshToken() {
     }),
   });
 
+  const data = await response.json();
+
   if (!response.ok) {
-    const data = await response.json();
-    console.log(data);
     window.location.href = "../index.html";
     return;
   }
 
-  const data = await response.json();
-  console.log("New access token" + data["new_access_token"]);
   localStorage.setItem("token", data["new_access_token"]);
-  location.reload();
+  loadDecks(true);
 }
 
-async function loadDecks() {
-  const response = await fetch("http://localhost:8000/decks/", {
-    method: "GET",
-    headers: { Authorization: "Bearer " + localStorage.getItem("token") },
-  });
+async function loadDecks(retried = false) {
+  try {
+    const response = await fetch("http://localhost:8000/decks/", {
+      method: "GET",
+      headers: { Authorization: "Bearer " + localStorage.getItem("token") },
+    });
 
-  if (response.status == 401) {
-    refreshToken();
-    console.log("Refresh Successful");
+    if (response.status == 401) {
+      if (retried) {
+        window.location.href = "../index.html";
+        return;
+      }
+
+      return await refreshToken();
+    }
+
+    const decks = await response.json();
+    const deccksCountText = document.getElementById("decks-count");
+    deccksCountText.textContent = decks.length + " decks";
+  } catch (error) {
+    console.error(error.message);
   }
-
-  const decks = await response.json();
-
-  const decksCountText = document.getElementById("decks-count");
-  decksCountText.textContent = decks.length + " decks";
-
-  console.log(decks);
 }
 
 loadDecks();
-//
-
-// async function loadDecks() {
-//   try {
-//     const response = await fetch("http://localhost:8000/decks/", {
-//       method: "GET",
-//       headers: { Authorization: "Bearer " + localStorage.getItem("token") },
-//     });
-//
-//     if (!response.ok) {
-//       throw new Error(`Response status: ${response.status}`);
-//     }
-//
-//     const decks = await response.json();
-//     const deccksCountText = document.getElementById("decks-count");
-//     deccksCountText.textContent = decks.length + " decks";
-//   } catch (error) {
-//     console.error(error.message);
-//   }
-// }
-//
-// loadDecks();
